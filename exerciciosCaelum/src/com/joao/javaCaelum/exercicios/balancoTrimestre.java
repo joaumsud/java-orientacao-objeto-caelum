14b2e9d93fd81ed58a5f3c7d6af6600d5ac6d320
joaumsud <joaumsud@hotmail.com>
2021-09-29 11:22:58 -0300

    Eercicios Caelum Cap 3

---
+++ exerciciosCaelum/src/com/joao/javaCaelum/exercicios/balancoTrimestre.java
@@ -0,0 +1,16 @@
+package com.joao.javaCaelum.exercicios;
+
+public class balancoTrimestre {
+
+	public static void main(String[] args) {
+		double janeiro = 15000.00;
+		double fevereiro = 23000.00;
+		double marco = 17000.00;
+		
+		double balanco = janeiro + fevereiro + marco;
+		
+		System.out.println("O valor gasto no primeiro trimestre é: " + balanco);
+
+	}
+
+}
---
+++ exerciciosCaelum/src/com/joao/javaCaelum/exercicios/imprime150.java
@@ -0,0 +1,11 @@
+package com.joao.javaCaelum.exercicios;
+
+public class imprime150 {
+	public static void main(String[] args) {
+		
+		int numero = 150;
+		while(numero <= 300) {
+			System.out.println(numero++);
+		}
+	}
+}
